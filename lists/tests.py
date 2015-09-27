27d5ff9bd09589f49b07d8a7af13b0904a37e780
Jesse Chiem <jessemai@gmail.com>
2015-09-27 16:01:15 -0700

    First ORM unit testing w/ building first database migration

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page  # home_page in lists/view.py
+from lists.models import Item
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -38,3 +39,26 @@ class HomePageTest(TestCase):
             {'new_item_text': 'A new list item'}
         )
         self.assertEqual(response.content.decode(), expected_html)
+
+class ItemModelTest(TestCase):
+    ''' new test model to create new records in database, and first
+    unit test for using Django's Object-Relational Mapper (ORM). '''
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        # .objects is a class attribute of the database
+        # .all() is a simple query to retrieve all records for table
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        
