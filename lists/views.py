90cac95de63ddbe2a6ba26f325ba6ceec4dc0c35
Jesse Chiem <jessemai@gmail.com>
2015-09-28 20:07:51 -0700

    Adjust lists/views for testing saving POST request to database

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
     ''' render takes the request as first parameter and
     name of the template to render.'''
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
     return render(request, 'home.html', {
         'new_item_text': request.POST.get('item_text', ''),
     })
